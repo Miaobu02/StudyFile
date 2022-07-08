1b9b2408560c429a7906b834b638fb2841aa5cca
Miaobu02 <104801852+Miaobu02@users.noreply.github.com>
2022-07-08 15:01:12 +0800

    Create 滑动窗口.java
    有关滑动窗口的刷题记录

---
+++ javaStudy/leetcode/滑动窗口.java
@@ -0,0 +1,37 @@
+// 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+// 换句话说，s1 的排列之一是 s2 的 子串 。
+// 示例 1：
+// 输入：s1 = "ab" s2 = "eidbaooo"
+// 输出：true
+// 解释：s2 包含 s1 的排列之一 ("ba").
+
+// 示例 2：
+// 输入：s1= "ab" s2 = "eidboaoo"
+// 输出：false
+
+//通过两个数组记录窗口所有字符串出现的次数是否和目标字符串的出现次数相同
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        vector<int> cnt1(26), cnt2(26);
+        for (int i = 0; i < n; ++i) {
+            ++cnt1[s1[i] - 'a'];
+            ++cnt2[s2[i] - 'a'];
+        }
+        if (cnt1 == cnt2) {
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            ++cnt2[s2[i] - 'a'];
+            --cnt2[s2[i - n] - 'a'];
+            if (cnt1 == cnt2) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
